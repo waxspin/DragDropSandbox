6f86c13988640265f6854ade41f858858ea5a2b8
Ben Bryan <wax.spin@gmail.com>
2011-09-03 23:48:56 -0500

    Some minor UI changes.

--- DragDropSandbox/debug/moc_dragdropsandbox.cpp
+++ DragDropSandbox/debug/moc_dragdropsandbox.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'dragdropsandbox.h'
 **
-** Created: Sat Sep 3 13:28:31 2011
+** Created: Sat Sep 3 23:48:34 2011
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.7.2)
 **
 ** WARNING! All changes made in this file will be lost!
