d8bad23174851bc482cefdd380a94148b81d0a9b
Ben Bryan <wax.spin@gmail.com>
2011-09-05 11:58:14 -0500

    Incremental commit, so I can test pull from GitHub.

--- DragDropSandbox/debug/moc_dragdropsandbox.cpp
+++ DragDropSandbox/debug/moc_dragdropsandbox.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'dragdropsandbox.h'
 **
-** Created: Sat Sep 3 23:48:34 2011
+** Created: Sun Sep 4 13:17:07 2011
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.7.2)
 **
 ** WARNING! All changes made in this file will be lost!
--- DragDropSandbox/ui_dragdropsandbox.h
+++ DragDropSandbox/ui_dragdropsandbox.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'dragdropsandbox.ui'
 **
-** Created: Sat Sep 3 23:48:32 2011
+** Created: Sun Sep 4 13:17:04 2011
 **      by: Qt User Interface Compiler version 4.7.2
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
