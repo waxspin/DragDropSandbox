50356349e1c5bcc5725e9f259c58a29eda8fbaa8
Ben Bryan <wax.spin@gmail.com>
2011-09-10 13:55:05 -0500

    About wired up.

--- DragDropSandbox/dragdropsandbox.cpp
+++ DragDropSandbox/dragdropsandbox.cpp
@@ -3,21 +3,23 @@
 using namespace log4cxx;
 using namespace log4cxx::helpers;
 
-LoggerPtr sandboxLogger(Logger::getLogger( "DragDropSandbox"));
+LoggerPtr sandboxLogger(Logger::getLogger("DragDropSandbox"));
 
-DragDropSandbox::DragDropSandbox(QWidget *parent)
-    : QMainWindow(parent)
-{
+DragDropSandbox::DragDropSandbox(QWidget *parent) :
+	QMainWindow(parent) {
 	ui.setupUi(this);
 	BasicConfigurator::configure();
-	LOG4CXX_INFO(sandboxLogger, "UI Setup completed.");
+	LOG4CXX_INFO(sandboxLogger, "UI Setup completed and logging initialized.");
+
+	//Setup the signal/slot connections.
+	connect(this->ui.actionAbout, SIGNAL(triggered()), this,
+			SLOT(aboutClicked()));
 }
 
-void DragDropSandbox::aboutClicked()
-{
+void DragDropSandbox::aboutClicked() {
+	LOG4CXX_INFO(sandboxLogger, "About triggered.");
 }
 
-DragDropSandbox::~DragDropSandbox()
-{
+DragDropSandbox::~DragDropSandbox() {
 
 }
