14202fe171db7447ec7470bfcf68d907b9a1376d
Ben Bryan <wax.spin@gmail.com>
2011-09-13 00:03:08 -0500

    Working on some UI changes.

--- DragDropSandbox/debug/moc_aboutdialog.cpp
+++ DragDropSandbox/debug/moc_aboutdialog.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'aboutdialog.h'
 **
-** Created: Mon Sep 12 23:21:34 2011
+** Created: Tue Sep 13 00:02:48 2011
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.7.2)
 **
 ** WARNING! All changes made in this file will be lost!
